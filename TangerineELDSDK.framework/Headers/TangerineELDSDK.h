1b196cc11618bf0862fc4fabcd0ee64dfecfc03a
Suraj <suraj.singh@tangerine.ai>
2021-06-29 16:43:50 +0530

    Added Event Type.

---
+++ TangerineELDSDK.framework/Headers/TangerineELDSDK.h
@@ -0,0 +1,18 @@
+//
+//  TangerineELDSDK.h
+//  TangerineELDSDK
+//
+//  Created by MAC PRO on 13/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TangerineELDSDK.
+FOUNDATION_EXPORT double TangerineELDSDKVersionNumber;
+
+//! Project version string for TangerineELDSDK.
+FOUNDATION_EXPORT const unsigned char TangerineELDSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TangerineELDSDK/PublicHeader.h>
+
+
